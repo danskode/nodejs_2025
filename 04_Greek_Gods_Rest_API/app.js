f2c67bc85e4705fc72d09d16deda16cf0ccdb265
danskode <149320369+danskode@users.noreply.github.com>
2025-09-15 14:29:14 +0200

    Added restapi endpoints

--- 04_Greek_Gods_Rest_API/app.js
+++ 04_Greek_Gods_Rest_API/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require( "express" );
 const app = express();
 
 const greekGods = [
@@ -13,28 +13,59 @@ const greekGods = [
     }
 ];
 
+// Body paresing ...
+app.use( express.json() );
+
 // (req, res) er en callback-metode ...
 // res.json er legacy! Vi skal bruge res.send ({ data: ...}). 
 // Det var noget med headers i gamle dage.
 // "data" kommer fra GraphQL, og gør data mere generisk og anvendelige for andre – husk {} for json
-app.get('/greekgods', (req,res) => {
+
+app.get('/greekgods', ( req,res ) => {
     res.send( {data: greekGods} );
 });
 
 app.get( '/greekgods/:id', ( req,res ) => {
     const providedGreekGodsID = Number( req.params.id );
-    const foundGreekGod = greekGods.find( ( greekGod ) => greekGod.id === providedGreekGodsID );
+    const foundGreekGod = greekGods.find( (greekGod) => greekGod.id === providedGreekGodsID );
 
     if ( !foundGreekGod) {
         res.status(404).send( {errorMessage: "Greek god not found ..."} );
     } else {
         res.send( { data: foundGreekGod } );
     }
-
 } );
 
+app.post('/greekgods', (req,res) =>{
+    res.send(req.body);
+});
 
-// til næste uge, resten af restapi'et
+app.put('/greekgods/:id', (req,res) => {
+    // find god
+    const providedGreekGodsID = Number( req.params.id );
+    const foundGreekGod = greekGods.find( (greekGod) => greekGod.id === providedGreekGodsID );
+    
+    if(!foundGreekGod){
+        res.status(404).send( {errorMessage: "No Greek god found with that id"} )
+    } else {
+        // Update god
+        res.send(req.body);
+    }
+});
+
+app.delete('/greekgods/:id', (req,res) => {
+    const providedGreekGodsID = Number( req.params.id );
+    const foundGreekGod = greekGods.find( (greekGod) => greekGod.id === providedGreekGodsID );
+    const indexReturned =  greekGods.findIndex( (greekGod) => greekGod.id === providedGreekGodsID );
+
+    if(!foundGreekGod){
+        res.status(404).send( {errorMessage: 'No Greek god found with that id'} );
+    } else {
+        greekGods.splice( indexReturned, 1);
+        res.send(`Greek god ${foundGreekGod.name} is deleted.`);
+        console.log(greekGods);
+    }
+});
 
 // 8080 til backend udvikling HTTP, 9090 til HTTPs
 // 3000 er til frontend udvikling
