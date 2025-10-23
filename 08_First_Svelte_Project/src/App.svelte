92a7c01177c7cce29cccd0b33482d9d485841bc2
danskode <149320369+danskode@users.noreply.github.com>
2025-10-23 18:09:01 +0200

    Class finished. New project in svelte added.

---
+++ 08_First_Svelte_Project/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Counter from "./components/Counter/Counter.svelte";
+    import Pokemon from "./components/Pokemon/Pokemon.svelte";
+</script>
+<h1>Welcome to our project</h1>
+<Counter/>
+<Pokemon/>
