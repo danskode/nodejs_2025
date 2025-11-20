504095db0aaac575a216202e0f160cecced0a239
danskode <149320369+danskode@users.noreply.github.com>
2025-11-20 11:26:25 +0100

    databaseundervisning før pause 2

--- 12_Database/routers/usersRouter.js
+++ 12_Database/routers/usersRouter.js
@@ -4,14 +4,9 @@ import db from '../database/connection.js';
 const router = Router();
 
 router.post('/api/users', async (req, res) => {
-
-    // const result = db.run('INSERT INTO users (username, role) VALUES (?, ?);', 
-    //    [req.body.username, req.body.role]);
-
     const { username, role } = req.body;
     const { lastID } = await db.run('INSERT INTO users (username, role) VALUES (?, ?);', [username, role]);
     res.send({ data: { createdId: lastID } });
-
 })
 
 export default router;
