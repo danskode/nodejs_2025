97a54beeb95b5f0f3c7e30337b840aa886d1c647
danskode <149320369+danskode@users.noreply.github.com>
2025-11-20 11:47:21 +0100

    databaseundervisning slut

--- 12_Database/app.js
+++ 12_Database/app.js
@@ -1,13 +1,26 @@
 import express from 'express';
 
 const app = express();
+
+app.use(express.static('public'));
+
 app.use(express.json());
 
 import exercisesRouter from './routers/exercisesRouter.js';
 app.use(exercisesRouter);
-
 import usersRouter from './routers/usersRouter.js';
 app.use(usersRouter);
+
+// Multer code ...
+import multer from 'multer';
+const upload = multer({
+    dest: './uploads'
+}
+);
+
+app.post('/profile', (req, res) => {
+    res.send({ data: "Image uploaded" });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- 12_Database/routers/usersRouter.js
+++ 12_Database/routers/usersRouter.js
@@ -7,6 +7,6 @@ router.post('/api/users', async (req, res) => {
     const { username, role } = req.body;
     const { lastID } = await db.run('INSERT INTO users (username, role) VALUES (?, ?);', [username, role]);
     res.send({ data: { createdId: lastID } });
-})
+});
 
 export default router;
